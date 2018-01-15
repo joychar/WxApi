136b8c6b4d069661df5b67767e90fc4e1f5fbf0c
joychar <panda_lqc@163.com>
2018-01-15 17:37:42 +0800

    message

---
+++ WxApi/Controllers/APIControllers/MessageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace WxApi.Controllers.APIControllers
+{
+    public class MessageController : ApiController
+    {
+        public HttpResponseMessage Post(string ToUserName, string FromUserName, int CreateTime, string MsgType, string Content, Int64 MsgId)
+        {
+
+
+            return new HttpResponseMessage { Content = new StringContent("ToUserName:"+ ToUserName, Encoding.GetEncoding("UTF-8"), "text/plain") };
+        }
+    }
+}
