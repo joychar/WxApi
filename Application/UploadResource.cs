ead4580abf2d5f07521901ae0b2ee79a212c7ab7
joychar <panda_lqc@163.com>
2018-02-07 09:21:04 +0800

    imgMessage

---
+++ Application/UploadResource.cs
@@ -0,0 +1,39 @@
+﻿using Common;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application
+{
+    public class UploadResource
+    {
+        public bool UploadPicture()
+        {
+            string uploadUrl = "";
+            string token = "";
+
+            //{
+            //    "articles": [{
+            //        "title": TITLE,
+            //        "thumb_media_id": THUMB_MEDIA_ID,
+            //        "author": AUTHOR,
+            //        "digest": DIGEST,
+            //        "show_cover_pic": SHOW_COVER_PIC(0 / 1),
+            //        "content": CONTENT,
+            //        "content_source_url": CONTENT_SOURCE_URL
+            //          },
+            //    //若新增的是多图文素材，则此处应还有几段articles结构
+            //    ]
+            //}
+
+            Dictionary<string, string> param = new Dictionary<string, string>();
+
+            string picContent = "";
+            param.Add("articles", picContent);
+            string data = HttpHelper.HttpPostData(string.Format(uploadUrl, token), param, Encoding.GetEncoding("UTF-8"));
+
+
+            return true;
+        }
+
+    }
+}
---
+++ Model/PostModel/WeChatArticleModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.PostModel
+{
+    public class WeChatArticleModel
+    {
+        public string titile { get; set; }
+        public string thumb_media_id { get; set; }
+        public string author { get; set; }
+        public string digest { get; set; }
+        public string show_cover_pic { get; set; }
+        public string content { get; set; }
+        public string content_source_url { get; set; }
+    }
+}
