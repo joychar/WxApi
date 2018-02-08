788647a549472d3ba6ab7dfc43b015b780e90858
joychar <panda_lqc@163.com>
2018-02-08 18:01:13 +0800

    bulid turingmsg

--- Application/TuringRebotRequest.cs
+++ Application/TuringRebotRequest.cs
@@ -1,4 +1,6 @@
 ﻿using Common;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,10 @@ namespace Application
         public static string AskTuring(string user, string content)
         {
             Dictionary<string, string> param = new Dictionary<string, string>();
+            param.Add("key", turingApiKey);
+            param.Add("info", content);
+            param.Add("userid", user);
+
             bool trans = true;
             if (trans)
             {
@@ -23,7 +29,31 @@ namespace Application
             else
             {
                 string data = HttpHelper.HttpPostData(requestUrl, param, Encoding.GetEncoding("UTF8"));
-                return "";
+
+                var turingMsg = JsonConvert.DeserializeObject(data) as JObject;
+                int msgCode = (int)turingMsg["code"];
+
+                string turingSay = string.Empty;
+                switch (msgCode)
+                {
+                    case 100000://文本消息
+                        turingSay = "文本消息";
+                        break;
+                    case 200000://链接消息
+                        turingSay = "链接消息";
+                        break;
+                    case 302000://新闻消息
+                        turingSay = "新闻消息";
+                        break;
+                    case 308000://菜谱消息
+                        turingSay = "菜谱消息";
+                        break;
+                    default:
+                        turingSay = "胖胖就是看着你，不想说话。";
+                        break;
+                }
+
+                return turingSay;
             }            
         }
     }
